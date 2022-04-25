f876bb9aaa6d6e1e80b0e1f7e1fee6d7a6636217
ramadantu <rvelisha@tu-sofia.bg>
2022-04-25 16:29:21 +0300

    Add junit tests

---
+++ src/test/java/com/internship/project/calculating/RpnCalculatorTest.java
@@ -0,0 +1,38 @@
+package com.internship.project.calculating;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RpnCalculatorTest {
+
+    @Test
+    @DisplayName("Checking for correctly done RPN calculation!")
+    void shouldCorrectlyCalculateRPN() {
+
+        RpnCalculator calculator = new RpnCalculator();
+        Queue<String> rpnQueue = new LinkedList<>();
+        rpnQueue.add("45");
+        rpnQueue.add("34");
+        rpnQueue.add("-");
+        rpnQueue.add("2");
+        rpnQueue.add("*");
+        rpnQueue.add("6");
+        rpnQueue.add("+");
+        rpnQueue.add("7");
+        rpnQueue.add("/");
+        rpnQueue.add("=");
+        Queue<String> expectedResult = new LinkedList<>();
+        expectedResult.add("4.0");
+
+        Queue<String> actualResult = calculator.calculate(rpnQueue);
+
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+}
